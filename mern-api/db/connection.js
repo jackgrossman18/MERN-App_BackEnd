3e31b073dd3408a7ed509854d36a6982da82bfcd
Jack Grossman <jackgrossman@Jacks-MacBook-Pro.local>
2018-07-16 22:20:26 -0400

    Typo corrected

--- mern-api/db/connection.js
+++ mern-api/db/connection.js
@@ -3,6 +3,6 @@ const mongoose = require("mongoose");
 if (process.env.NODE_ENV === "production") {
   mongoose.connect(process.env.MLAB_URL);
 } else {
-  mongoose.connect("mongodb://localhost:27017/heir-pollution");
+  mongoose.connect("mongodb://localhost:27017/evertonfc_db");
 }
 module.exports = mongoose;
